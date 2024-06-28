167c197a19df348ff00aeded26e3394babe23c41
Rahil Ansari <rahilansari261@gmail.com>
2024-06-28 17:36:21 +0530

    fixes

---
+++ utils/api.js
@@ -0,0 +1,4 @@
+export const apiToken = "";
+export const XRapidAPIKey = "";
+export const XRapidAPIHost = "coinranking1.p.rapidapi.com";
+export const XRapidAPIHostNews = "cryptocurrency-news2.p.rapidapi.com";
